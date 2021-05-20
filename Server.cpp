a802286117ff703d121f542c68cf267fde85e043
Roma-x <tsymbaljuk2001@gmail.com>
2021-05-20 17:09:09 +0300

    feature: Added multithreaded version with blocking whole table

--- Server.cpp
+++ Server.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <chrono>
 #include <vector>
+#include <mutex>
 #include <thread>
 #include <fstream>
-#include <map>
+#include <unordered_map>
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
@@ -12,7 +14,11 @@
 #include <dirent.h>
 #include <algorithm>
 #define PORT 8080
-#define THREADS_NUMBER 4;
+#define THREADS_NUMBER 3
+std::unordered_map<std::string, std::vector<std::string>> table;
+std::vector<std::string> ignorlist{".", ",","!","@","#","$","%","&","*","(",")", "_","+","=","?","`","~", "|","/", ":", ";", "<", ">", "{", "}"};
+std::vector<std::string> ignorWords{"br", "</br>", "<h>", "</h>"};
+
 std::vector<std::string> getFiles(char* path, std::string startIndex, std::string endIndex){
 
     
@@ -62,12 +68,127 @@ std::vector<std::string> getFiles(char* path, std::string startIndex, std::strin
 
 }
 
+std::mutex mtx;
+
+void buildIndex(std::vector<std::string> temp,  char* path){
+
+
+    
+
+    for (std::vector<std::string>::iterator ik=temp.begin(); ik != temp.end(); ++ik){
+
+
+
+        std::string filename(path);
+        filename.append("/");
+        filename.append((*ik));
+        std::string f = (*ik);
+
+        std::fstream file;
+        file.open(filename);
+
+        if (!file) {
+
+            std::cout << "No such file"<<std::endl;
+
+        }
+
+        else {
+
+            std::string word;
+
+            while (1) {
+
+                file >> word;
+
+                for (std::vector<std::string>::iterator it = ignorWords.begin(); it!=ignorWords.end(); ++it){
+
+
+                    std::string::size_type i = word.find((*it));
+
+                    if (i != std::string::npos)
+                        word.erase(i, word.length());
+                
+                }
+
+                for (std::vector<std::string>::iterator it = ignorlist.begin(); it!=ignorlist.end(); ++it){
+
+                    if (word.find((*it)) != std::string::npos) {
+
+                        char a = (*it)[0];
+
+                        word.erase(std::remove(word.begin(), word.end(), a), word.end());
+
+                    }
+                    
+
+                }
+                
+//                std::mutex mtx;
+
+                if ( table.find(word) == table.end() ) {
+
+                    std::lock_guard<std::mutex> guard(mtx);
+
+                    std::vector<std::string> key;                    
+                    key.push_back(f);
+                    std::thread::id this_id = std::this_thread::get_id();
+                    
+//                    std::cout<<"Word : "<<word<<"  found  "<<" by thread : "<<this_id<<std::endl;
+                    
+                    table.insert({word, key});
+                    
+
+                } 
+
+                else {
+
+                    int exists = 0;
+
+                    std::lock_guard<std::mutex> guard(mtx);
+                    
+
+                    for (std::vector<std::string>::iterator it = (table.find(word)->second).begin(); it!= (table.find(word)->second).end(); ++it){
+                        if ((*it) == f){
+                            exists = 1;
+                            break;
+                        }
+                    }
+
+                    if (!exists){
+
+                        (table.find(word)->second).push_back(f);
+                        
+                    }
+
+                }
+                
+                if (file.eof()){
+
+                    file.close();
+                    break;
+
+
+                    }
+
+                }
+            }
+
+        }
+
+
+   
+    }
+
+
 int main(int argc, char const *argv[])
 {
     char* path = "./aclImdb_v1/aclImdb/train/neg";
-    std::string startIndex = "7750";
+
+    std::string startIndex = "6000"; //7750
     std::string endIndex = "8000";
     std::vector<std::string> v;
+    std::vector<std::thread> threads;
 
     v = getFiles(path, startIndex, endIndex);
 
@@ -76,116 +197,46 @@ int main(int argc, char const *argv[])
     int opt = 1;
     int addrlen = sizeof(address);
     
-
-    std::map<std::string, std::vector<std::string>> table;
+    char symbol = '"';
+    std::string s(1, symbol); 
+    ignorlist.push_back(s);
     
-    std::vector<std::string> ignorlist{".", ",","!","@","#","$","%","&","*","(",")", "_","+","=","?","`","~", "|","/", ":", ";", "<", ">", "{", "}"};
-    std::vector<std::string> ignorWords{"br", "</br>", "<h>", "</h>"};
+    int i = 0;
+std::cout<<"v size "<<v.size()<<std::endl;
+std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-    for (std::vector<std::string>::iterator it = v.begin(); it!=v.end(); ++it){
+    for (int i = 0; i < THREADS_NUMBER; i++){
 
-        std::fstream file;
-        std::string filename(path);
-        filename.append("/");
-        filename.append((*it));
-        std::string f = (*it);
-        file.open(filename, std::ios::in);
-        if (!file) {
-            std::cout << "No such file"<<std::endl;
+
+
+        std::vector<std::string> temp;
+        int start = (v.size() / THREADS_NUMBER ) * i;
+        int end = (v.size()  / THREADS_NUMBER ) * (i + 1);
+
+        for (int k = start; k < end; k++ ){
+            temp.push_back(v[k]);
         }
-        else {
 
-            std::string word;
+        threads.push_back(std::thread(buildIndex, temp, path));
 
-        while (1) {
+        temp.clear();
 
-            file >> word;
-
-            
-
-            for (std::vector<std::string>::iterator it = ignorWords.begin(); it!=ignorWords.end(); ++it){
-
-
-                std::string::size_type i = word.find((*it));
-
-                if (i != std::string::npos)
-                    word.erase(i, word.length());
-            
-            }
-
-            for (std::vector<std::string>::iterator it = ignorlist.begin(); it!=ignorlist.end(); ++it){
-
-                if (word.find((*it)) != std::string::npos) {
-
-                    char a = (*it)[0];
-
-                    word.erase(std::remove(word.begin(), word.end(), a), word.end());
-
-                }
-                
-
-            }
-            
-
-
-            if ( table.find(word) == table.end() ) {
-
-                std::vector<std::string> key;
-                
-                
-                key.push_back(f);
-
-                table.insert({word, key});
-  
-            } else {
-
-                int exists = 0;
-                for (std::vector<std::string>::iterator it = (table.find(word)->second).begin(); it!= (table.find(word)->second).end(); ++it){
-                    if ((*it) == f){
-                        exists = 1;
-                        break;
-                    }
-                }
-                if (!exists){
-                    (table.find(word)->second).push_back(f);
-                }
-
-            }
-            
-
-
-            if (file.eof()){
-
-                file.close();
-                break;
-            }
-
-            }
-        }
     }
 
-    std::map<std::string, std::vector<std::string>>::iterator it;
-//    for (it = table.begin(); it != table.end(); it++)
-//    {
 
-//    std::cout << it->first<<':';
-//        for (std::vector<std::string>::iterator ig = (it->second).begin(); ig!=(it->second).end(); ++ig){
+    for (auto& th : threads) th.join();
 
-//       std::cout<<(*ig)<<"  ";
+std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-//        }
-//    printf("\n");
+std::cout<<"Index built in "<<std::chrono::duration_cast<std::chrono::seconds>(end - begin).count()<<" seconds "<<std::endl;
 
-//    }
-
-    // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
        
-    // Forcefully attaching socket to the port 8080
+
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                                                   &opt, sizeof(opt)))
     {
@@ -196,7 +247,7 @@ int main(int argc, char const *argv[])
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
        
-    // Forcefully attaching socket to the port 8080
+
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
     {
         perror("bind failed");
@@ -217,10 +268,6 @@ int main(int argc, char const *argv[])
     char buffer[1024] = {0};
     valread = read(new_socket, buffer, 1024);
 
-
-//    printf("\nGot from client : %s\n", buffer);  
-//    printf("\n\n Printing our map \n\n");
-    
     std::string input(buffer);
     auto found = table.find(input);
 
@@ -233,8 +280,6 @@ int main(int argc, char const *argv[])
     }
 
     std::cout<<std::endl;
-
-
     
     return 0;
 }
